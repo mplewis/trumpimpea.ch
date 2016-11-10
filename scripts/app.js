4cea04fc1b8f816c2b4cb1059ecd10ba120eb16b
Matthew Lewis <matt@mplewis.com>
2016-11-09 22:14:38 -0700

    git: initial commit

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+/* eslint-env jquery */
+
+// Jan 20, 2017, 12:00 PM EST
+var inaugOn = new Date('2017-01-20T12:00:00.00-0900')
+var preInaug = 'Trump will be sworn into office in'
+var postInaug = [
+  'Trump has been in office for',
+  'without being impeached'
+]
+
+function countUp () {
+  $('.top').text(postInaug[0])
+  $('.bottom').text(postInaug[1])
+
+  var since = (inaugOn - new Date()) / 1000
+  $('.clock').FlipClock(since, {
+    clockFace: 'DailyCounter'
+  })
+}
+
+function countDown () {
+  $('.top').text(preInaug)
+
+  var until = (inaugOn - new Date()) / 1000
+  $('.clock').FlipClock(until, {
+    clockFace: 'DailyCounter',
+    countdown: true
+  })
+}
+
+// mutate the page only if Trump has taken office
+var now = new Date()
+if (now - inaugOn >= 0) countUp(); else countDown()

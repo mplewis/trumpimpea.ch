d164a1b755987a238c3630d48ec88fa5f641a832
Matt Lewis <matt@mplewis.com>
2017-06-10 20:49:45 -0600

    refactor: simplify, remove external deps

--- scripts/app.js
+++ scripts/app.js
@@ -2,31 +2,20 @@
 
 // Jan 20, 2017, 12:00 PM EST
 var inaugOn = new Date('2017-01-20T12:00:00.00-0900')
-var preInaug = 'Trump will be sworn into office in'
-var postInaug = [
-  'Trump has been in office for',
-  'without being impeached'
-]
 
-function countUp () {
-  $('.top').text(postInaug[0])
-  $('.bottom').text(postInaug[1])
-
-  var since = (inaugOn - new Date()) / 1000
-  $('.clock').FlipClock(since, {
-    clockFace: 'DailyCounter'
-  })
+function updateCount () {
+  const diff = (new Date() - inaugOn) / 1000
+  const secs = parseInt(diff % 60)
+  const mins = parseInt((diff / 60) % 60)
+  const hours = parseInt((diff / 60 / 60) % 24)
+  const days = parseInt(diff / 60 / 60 / 24)
+  const ss = secs === 1 ? '' : 's'
+  const ms = mins === 1 ? '' : 's'
+  const hs = hours === 1 ? '' : 's'
+  const ds = days === 1 ? '' : 's'
+  const msg = `${days} day${ds}, ${hours} hour${hs}, ${mins} minute${ms}, and ${secs} second${ss}`
+  document.querySelector('.clock').innerText = msg
 }
 
-function countDown () {
-  $('.top').text(preInaug)
-
-  var until = (inaugOn - new Date()) / 1000
-  $('.clock').FlipClock(until, {
-    clockFace: 'DailyCounter',
-    countdown: true
-  })
-}
-
-var now = new Date()
-if (now - inaugOn >= 0) countUp(); else countDown()
+setInterval(updateCount, 1000);
+updateCount();
